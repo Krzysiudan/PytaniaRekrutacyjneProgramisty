98fc8afc8a4797fabc4051b957b441f3627771c8
Krzysiudan <krzysiudan@gmail.com>
2019-10-24 20:30:23 +0200

    Added activity for questions and answers

---
+++ app/src/main/java/com/krzysiudan/pytaniarekrutacyjne/ActivitySingleQuestion.java
@@ -0,0 +1,13 @@
+package com.krzysiudan.pytaniarekrutacyjne;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySingleQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_question);
+    }
+}
